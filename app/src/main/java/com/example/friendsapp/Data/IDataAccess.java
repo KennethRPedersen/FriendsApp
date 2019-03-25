28143f08fc35084ba5496e45365ab2783fe4462a
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2019-03-25 13:47:24 +0100

    Added a IDataAccess interface with CRUD for friends

---
+++ app/src/main/java/com/example/friendsapp/Data/IDataAccess.java
@@ -0,0 +1,13 @@
+package com.example.friendsapp.Data;
+
+import com.example.friendsapp.BE.BEFriend;
+
+import java.util.List;
+
+public interface IDataAccess {
+    List<BEFriend> getAllFriends();
+    BEFriend getFriendById(int id);
+    void deleteFriend(int id);
+    BEFriend updateFriend(BEFriend updatedFriend);
+    BEFriend addFriend(BEFriend newFriend);
+}
