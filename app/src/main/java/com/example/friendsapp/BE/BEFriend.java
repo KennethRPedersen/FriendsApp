210ad9fa6b8ee676da8651bf1679229f626c8467
Kenneth P <quizandk@gmail.com>
2019-03-25 13:30:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/friendsapp/BE/BEFriend.java
@@ -0,0 +1,57 @@
+package com.example.friendsapp.BE;
+
+import java.util.Date;
+
+public class BEFriend {
+    String name, address, email, phoneNumber, website;
+    Date birthdate;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+}
