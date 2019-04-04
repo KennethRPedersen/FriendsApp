728d83b048b9f60a3326442faadffb0df33424c2
Kenneth P <quizandk@gmail.com>
2019-04-04 09:19:38 +0200

    Added date picker
    Added date picker to make it easier to pick a date in correct format.

--- app/src/main/java/com/example/friendsapp/View/DetailActivity.java
+++ app/src/main/java/com/example/friendsapp/View/DetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.friendsapp.View;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -13,6 +16,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -28,6 +32,7 @@ import com.example.friendsapp.Shared;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.sql.Date;
+import java.util.Calendar;
 
 public class DetailActivity extends AppCompatActivity implements IViewCallBack {
 
@@ -201,8 +206,34 @@ public class DetailActivity extends AppCompatActivity implements IViewCallBack {
                 cancel();
             }
         });
+
+        etBDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker();
+            }
+        });
+
     }
 
+    private void openDatePicker() {
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                android.R.style.Theme_Holo_Light_Dialog,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        etBDay.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+    
     private void cancel() {
         stopListener();
         finish();
