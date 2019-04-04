4c066ab156626115de650d051bdf36d5660971d5
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2019-04-04 09:45:22 +0200

    Cleanup

--- app/src/main/java/com/example/friendsapp/View/MapsActivity.java
+++ app/src/main/java/com/example/friendsapp/View/MapsActivity.java
@@ -19,13 +19,9 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private static final long DOUBLECLICK_TIME_THRESHOLD = 10000;
-    private GoogleMap mMap;
-    private BEFriend[] friends;
     private int DETAILACTIVITY_INTENT_ID = 1345;
-    private boolean markerClicked = false;
-    private long lastClickedId;
-    private long lastClickedTime;
+
+    private BEFriend[] friends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,22 +56,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         googleMap.getUiSettings().setZoomControlsEnabled(true);
 
         setHomeMarkersIn(googleMap);
+
         googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
             @Override
             public void onInfoWindowClick(Marker marker) {
@@ -86,6 +76,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(friends[0].getHome()));
     }
 
+    /**
+     * Set a marker on the given map for every friend in the friends array.
+     * @param map
+     */
     private void setHomeMarkersIn(GoogleMap map) {
         for (BEFriend friend:friends) {
             MarkerOptions markOpt = new MarkerOptions();
@@ -97,7 +91,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     /**
-     * Open a friend in detail view.
+     * Open a friend in detail view with the friend with the given id.
      * For adding a new friend give id -1.
      * @param id
      */
@@ -106,6 +100,4 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         intent.putExtra(Shared.ID_KEY, id);
         startActivityForResult(intent, DETAILACTIVITY_INTENT_ID);
     }
-
-
 }
